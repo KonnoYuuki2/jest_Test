edacca8f1c45d33532ce3d9a6bb5215a7810d84c
MinjaeK <93378322+KonnoYuuki2@users.noreply.github.com>
2025-04-17 18:37:20 +0900

    Update swagger
    auth 인증을 위한 security 추가

--- src/swagger/swagger.js
+++ src/swagger/swagger.js
@@ -1,23 +1,40 @@
 import swaggerUi from 'swagger-ui-express';
 import swaggereJsDoc from 'swagger-jsdoc';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const options = {
-    swaggerDefinition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'token test API',
-            version: '1.0.0',
-            description: 'Access / Refresh token test',
-        },
-        servers: [
-            {
-                url: 'http://localhost:5555',
-            },
-        ],
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'token test API',
+      version: '1.0.0',
+      description: 'Access / Refresh token test',
     },
-    
-    apis: ['./src/routers/*.js'],
-}
+    servers: [
+      {
+        url: dotenv.host + dotenv.port,
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ['./src/routers/*.js'], // 여긴 그대로 OK
+};
+
 
 const specs = swaggereJsDoc(options);
 
