0b540e709a341d2c6746c4e57b411286038c4676
MinjaeK <93378322+KonnoYuuki2@users.noreply.github.com>
2025-04-17 18:35:58 +0900

    Update: authorization
    authorization 인증 로직 수정 및 변경

--- middle_wares/authorization.js
+++ middle_wares/authorization.js
@@ -4,29 +4,57 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const authorization = async (req,res,next) => {
+    let result;
+
     if(req.headers.authorization) {
-        const token = req.headers.authorization.split('Bearer ');
+        const token = req.headers.authorization.split(' ');
 
-        const result = await jwt.verify(token[1],process.env.accessTokenKey);
-
-        if(result.exp <= Math.floor(Date.now() / 1000)) {
-            console.log(`토큰 만료 됨!`);
-
-            const refreshToken = jwt.sign({username:result.username}, process.env.refreshTokenKey, {expiresIn: '3h'});
-
-            res.status(401).send({token: refreshToken});
+        if(token[0] !== 'Bearer') {
+            return res.status(401).send({
+                 error: {
+                     code:"INVALID_TOKEN",
+                     message: "토큰이 유효하지 않습니다."
+                }
+            });
+        }
+        
+        try {
+            result = await jwt.verify(token[1],process.env.accessTokenKey);
+        }
+        catch(error) {
+            if(error.name === "TokenExpiredError") {
+                res.status(401).send({
+                    error:{
+                        code:"EXPIRED_TOKEN",
+                        message: "토큰이 만료되었습니다."
+                    }
+                });
+            }
+            else {
+                return res.status(401).send({
+                 error: {
+                     code:"INVALID_TOKEN",
+                     message: "토큰이 유효하지 않습니다."
+                }
+            });    
+            }
+            
         }
 
         if(result) {
            req.info = {
             username: result.username, 
-            password: result.password
           };
 
           next();
         }
     }
     else {
-        res.status(401).send("incorrected jwt Token");
+        res.status(401).send({
+            error: {
+                code: "TOKEN_NOT_FOUND",
+                message: "토큰이 없습니다."
+            }
+        });
     }
 }
